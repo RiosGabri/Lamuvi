d9530f38d60269d860cabd8548a8fea1bc44f79b
RiosGabri <grp@cesar.school>
2026-03-19 06:55:02 -0300

    Adição do views provisório para validação do usuário

--- Lamuvi/views.py
+++ Lamuvi/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+def cadastrar(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'sucesso.html') 
+        else:
+            messages.error(request, "Erro: Verifique os dados inseridos.")
+    
+    return render(request, 'cadastro.html')
