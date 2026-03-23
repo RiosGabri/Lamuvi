f6406f0ef7ee30c34d228fc0a3ab7459071ffbf4
RiosGabri <grp@cesar.school>
2026-03-23 14:43:14 -0300

    Adição de lista não finalizada de filmes

--- Lamuvi/js/data/lista.js
+++ Lamuvi/js/data/lista.js
@@ -1,1 +1,27 @@
 //lista de filmes
+export const Lista_filmes = [
+    {
+        id: 1,
+        nome: "Interestelar",
+        imagem: "assets/imagens/Interestelar.jpg",
+        diretor: "Christopher Nolan",
+        sinopse: "...",
+        ano_lançamento: "2014"
+    },
+    {
+        id: 2,
+        nome: "Homem Aranha: Através do Aranhaverso",
+        imagem: "assets/imagens/Homem_aranha.jpg",
+        diretor: "Justin K. Thompson, Kemp Powers & Joaquim Dos Santos",
+        sinopse: "...",
+        ano_lançamento: "2023"
+    },
+    {
+        id: 3,
+        nome: "A Noviça Rebelde",
+        imagem: "assets/imagens/A_noviça_rebelde.jpg",
+        diretor: "Robert Wise",
+        sinopse: "...",
+        ano_lançamento: "1965 "
+    },
+];
