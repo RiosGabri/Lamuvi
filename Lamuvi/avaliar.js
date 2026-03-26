ef0ecf57c31021a4bca121cb50ae8737a304cd30
RiosGabri <grp@cesar.school>
2026-03-26 10:42:57 -0300

    Commit p/ salvar modelo de avaliar

---
+++ Lamuvi/avaliar.js
@@ -0,0 +1,29 @@
+window.salvarAvaliacao = function () {
+    let usuario_nome = localStorage.getItem("Loginok");
+    if (!usuario_nome) {
+        window.location.href = "login.html";
+        return;
+    }
+    let filme_id = document.getElementById("filme_id").value;
+    let nota = document.getElementById("nota").value;
+    let comentario = document.getElementById("comentario").value;
+
+    if (filme_id === "" || nota === "") {
+        alert("Preencha os campos corretamente!");
+        return;
+    }
+    let avaliacoes = JSON.parse(localStorage.getItem("avaliacoes")) || {};
+
+    // Salva pelo ID do filme 
+    avaliacoes[filme_id] = {
+        nota: nota,
+        comentario: comentario
+    };
+
+    localStorage.setItem("avaliacoes", JSON.stringify(avaliacoes));
+
+    alert("Sua opnião foi salva com sucesso!");
+    // Limpa campos
+    document.getElementById("nota").value = "";
+    document.getElementById("comentario").value = "";
+};
--- Lamuvi/js/pages/login.js
+++ Lamuvi/js/pages/login.js
@@ -1,7 +1,7 @@
 window.entrar = function () { //Entrar na conta, window deixa a função global
     let usuario_nome = document.getElementById("usuario_nome").value; //Bote no html para digitar o nome e senha
     let senha_conta = document.getElementById("senha_conta").value;
-
+    
     if (usuario_nome === "IHCble123" && senha_conta === "5!vL") {
         localStorage.setItem("Loginok", usuario_nome); //Cria item para mostra que entrou na conta
         window.location.href = "filmes.html"; //Redireciona para tela principal
