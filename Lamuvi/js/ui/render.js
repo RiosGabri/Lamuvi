b93c1c65a179964650be83a4a64104204248ede8
RiosGabri <grp@cesar.school>
2026-03-26 08:31:10 -0300

    Implementei o módulo para abertura de filmes

--- Lamuvi/js/ui/render.js
+++ Lamuvi/js/ui/render.js
@@ -1,15 +1,14 @@
 //Exibição dos filmes da tela
 export function renderFilmes(lista) {
-    const container = document.getElementById("lista"); //Vai pegar o elemento lista do HTML(viu geoguesser? :D)
-
-    let html = ""; //Criei uma variável p/ armazenar código HTML (viu geoguesser? :D)
+    const container = document.getElementById("lista"); //Vai pegar o elemento lista do HTML
+    let html = ""; //Criei uma variável p/ armazenar código HTML
 
     lista.forEach(f => { //Olha, o forEach vai executar a função para cada filme e cria um card p/ cada
         //As crases é só p/ facilitar a vida, pois faz a interpolação
         //Vai criar um card p/ cada filme e busca pelo id do filme , depois busca o resto
         //Lembrando que isso é só protótipo
         html += `
-            <div class="card" onclick="abrirFilme(${f.id})"> 
+            <div class="card" onclick="abrirFilme(${f.id})">
                 <img src="${f.imagem}" width="150"> 
                 <h3>${f.nome}</h3>
                 <p>${f.genero}</p>
@@ -22,3 +21,7 @@ export function renderFilmes(lista) {
     container.innerHTML = html; //Depois do html surgir, coloca ele dentro de um container (ou seja, o elemento lista do html)
     //vi isso na internet, espero que funcione
 }
+window.abrirFilme = function (id) {
+    localStorage.setItem("Oescolhodehvc", id); //Abre o filme
+    window.location.href = "pagina_filme.html"; //Redirecionei para a página
+};
