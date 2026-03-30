fc0778e51f61ab2f9cbb97a43045e97c1538fe26
Gabriel Parméra <grp@cesar.school>
2026-03-30 07:09:28 -0300

    Update login.js

--- Lamuvi/js/login.js
+++ Lamuvi/js/login.js
@@ -1,26 +1,26 @@
-//window.entrar = function () {
-//    let usuario_nome = document.getElementById("usuario_nome").value;
-//    let senha_conta = document.getElementById("senha_conta").value;
-//    let extra = JSON.parse(localStorage.getItem("usuario_cadastrado"));
-
-//    if ((usuario_nome === "IHCble123" && senha_conta === "5!vL") || 
-//        (extra && usuario_nome === extra.nome && senha_conta === extra.senha)) { 
-//        localStorage.setItem("Loginok", usuario_nome); 
-//        window.location.href = "filmes.html"; 
-//    } else {
-//        document.getElementById("erro").innerText = "Usuário e/ou senha incorreto(s)!";
-//    }
-//}
-
 window.entrar = function () {
-    console.log("clicou no botão"); // 👈 TESTE
-
     let usuario_nome = document.getElementById("usuario_nome").value;
     let senha_conta = document.getElementById("senha_conta").value;
+    let extra = JSON.parse(localStorage.getItem("usuario_cadastrado"));
 
-    localStorage.setItem("Loginok", usuario_nome);
+    if ((usuario_nome === "IHCble123" && senha_conta === "5!vL") || 
+        (extra && usuario_nome === extra.nome && senha_conta === extra.senha)) { 
+        localStorage.setItem("Loginok", usuario_nome); 
+        window.location.href = "filmes.html"; 
+    } else {
+        document.getElementById("erro").innerText = "Usuário e/ou senha incorreto(s)!";
+   }
+}
 
-    console.log("vai redirecionar"); // 👈 TESTE
+//window.entrar = function () {
+//    console.log("clicou no botão"); 
 
-    window.location.href = "filmes.html";
+//    let usuario_nome = document.getElementById("usuario_nome").value;
+//    let senha_conta = document.getElementById("senha_conta").value;
+
+//    localStorage.setItem("Loginok", usuario_nome);
+
+//    console.log("vai redirecionar"); 
+
+//    window.location.href = "filmes.html";
 };
