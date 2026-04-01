8945664b93b67fcc40f184f9dbd948ed182c5eaa
eduardommb <lemb@cesar.school>
2026-04-01 18:25:55 -0300

    fix: refatora e padroniza notificacoes de cadastro

--- Lamuvi/js/cadastro.js
+++ Lamuvi/js/cadastro.js
@@ -1,15 +1,17 @@
-window.cadastrar = function () {
-    let nome = document.getElementById("novo_usuario").value;
-    let senha = document.getElementById("nova_senha").value;
-//Só vai criar um novo usuário -> Só para mostrar na aula
-    if (nome === "" || senha === "") {
-        alert("Preencha os campos!");
-        return;
-    }
-    // Salva apenas este novo usuário
-    let dadosUsuario = { nome: nome, senha: senha };
-    localStorage.setItem("usuario_cadastrado", JSON.stringify(dadosUsuario));
+window.cadastrar = function() {
+  let nome = document.getElementById("novo_usuario").value;
+  let senha = document.getElementById("nova_senha").value;
+  //Só vai criar um novo usuário -> Só para mostrar na aula
+  if (nome === "" || senha === "") {
+    mostrarNotificacao("Preencha todos os campos!", "erro");
+    return;
+  }
 
-    alert("Sua conta foi criada com sucesso! Agora faça o login.");
+  // Salva apenas este novo usuário
+  let dadosUsuario = { nome: nome, senha: senha };
+  localStorage.setItem("usuario_cadastrado", JSON.stringify(dadosUsuario));
+  mostrarNotificacao("Conta criada com sucesso!", "sucesso");
+  setTimeout(() => {
     window.location.href = "login.html";
+  }, 2000);
 };
