7c0747d958465f12d4ddd5e1ce1d0f2a3c5d3d91
RiosGabri <grp@cesar.school>
2026-04-02 06:31:50 -0300

    Termos de serviço

--- Lamuvi/js/cadastro.js
+++ Lamuvi/js/cadastro.js
@@ -1,13 +1,12 @@
 window.cadastrar = function() {
+  const checkboxTermos = document.getElementById('termos');
   let nome = document.getElementById("novo_usuario").value;
   let senha = document.getElementById("nova_senha").value;
-  //Só vai criar um novo usuário -> Só para mostrar na aula
   if (nome === "" || senha === "") {
     mostrarNotificacao("Preencha todos os campos!", "erro");
     return;
   }
 
-  // Salva apenas este novo usuário
   let dadosUsuario = { nome: nome, senha: senha };
   localStorage.setItem("usuario_cadastrado", JSON.stringify(dadosUsuario));
   mostrarNotificacao("Conta criada com sucesso!", "sucesso");
