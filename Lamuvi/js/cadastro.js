8e8bf3d1bd9f580d925425fdc6ef86ab3f4d0548
eduardommb <lemb@cesar.school>
2026-04-02 07:14:06 -0300

    update: cadastro agora necessita do checkbox

--- Lamuvi/js/cadastro.js
+++ Lamuvi/js/cadastro.js
@@ -2,8 +2,20 @@ window.cadastrar = function() {
   const checkboxTermos = document.getElementById('termos');
   let nome = document.getElementById("novo_usuario").value;
   let senha = document.getElementById("nova_senha").value;
+  let senhaConfirma = document.getElementById("senhaConfirma").value;
+
   if (nome === "" || senha === "") {
     mostrarNotificacao("Preencha todos os campos!", "erro");
+    return;
+  }
+
+  if (senha !== senhaConfirma) {
+    mostrarNotificacao("As senhas não coincidem!", "erro");
+    return;
+  }
+
+  if (!checkboxTermos.checked) {
+    mostrarNotificacao("Você precisa concordar com os Termos de Serviço!", "erro");
     return;
   }
 
